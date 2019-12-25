822f88ec4cb7fd352c921b99c943d6a57c1cbfb1
guo.feng <guo.feng@neusoft.com>
2019-12-25 22:20:43 +0800

    1

--- test/DataHub/DataHub_switch.js
+++ test/DataHub/DataHub_switch.js
@@ -19,8 +19,8 @@ const DataHub = require('../../lib/DataHub/DataHub.js').default;
 
 let fetchLogger = createLog('FetchLog', 'log', true);
 
-if (!hasInitFetcher()) {	
-	initFetcher(function (arg) {
+if (!hasInitFetcher()) {
+	initFetcher(function(arg) {
 		const {
 			url,
 			data,
@@ -28,20 +28,20 @@ if (!hasInitFetcher()) {
 			onStop,
 			stopKey,
 		} = arg;
-		
+
 		console.log('测试Switch：加载数据', url, data);
-		
+
 		if (url === 'url://test.fetch.switch.1') {
 			setResult();
 		}
-		
+
 	});
-	
+
 	addFetcher('test.fetch.switch', 'url://test.fetch.switch.1', 'get', {});
-	
+
 	let devLogger = createLog('TestDataHubFetch', 'log', true);
 	let errLogger = createLog('TestDataHubFetch', 'error', true);
-	
+
 	let dh1 = new DataHub({
 		test1: {
 			fetcher: 'test.fetch.switch',
@@ -57,30 +57,32 @@ if (!hasInitFetcher()) {
 			dependence: 'dep1',
 		}
 	}, devLogger, errLogger);
-	
+
 	let dh1c1 = dh1.getController();
-	
+
 	equalAssert('测试关闭状态', dh1c1.getSwitchStatus('test1'), false);
-	
+
 	equalAssert('测试打开状态', dh1c1.getSwitchStatus('test2'), true);
-	
+
 	equalAssert('测试不存在的开关', dh1c1.getSwitchStatus('test3'), null);
-	
+
 	let asyncEqualAssert1 = createAsyncEqualAssert();
-	
-	
+
+
 	asyncEqualAssert1('测试开关', (next) => {
 		dh1c1.turnOn('test1');
 		console.log('打开开关1');
 		next(dh1c1.getSwitchStatus('test1'), true);
 	}, 1000);
-	
+
 	asyncEqualAssert1('满足依赖', (next) => {
 		console.log('满足依赖');
-		dh1c1.set('dep1', {a: 1});
+		dh1c1.set('dep1', {
+			a: 1
+		});
 		next();
 	}, 100);
-	
+
 	asyncEqualAssert1('测试满足依赖的开关', (next) => {
 		console.log('打开开关2');
 		dh1c1.turnOn('test4');
