79b752b6354c8e624235ce958ae6bb4e6b082cbd
guo.feng <guo.feng@neusoft.com>
2020-03-02 21:46:37 +0800

    ui

--- src/DataHub/FetchManager.js
+++ src/DataHub/FetchManager.js
@@ -1,219 +1,223 @@
 import {
-	createUid,
-	udFun,
-	isNvl,
+  createUid,
+  udFun,
+  isNvl,
 } from './../Utils';
 
 import {
-	ABORT_REQUEST,
-	stopFetchData,
-	fetchData,
-	hasFetching
+  ABORT_REQUEST,
+  stopFetchData,
+  fetchData,
+  hasFetching
 } from './Fetcher';
 
 import {
-	getRefreshRate
+  getRefreshRate
 } from '../Common/Union';
+
+import Timer from '../Common/Timer';
 
 import Component from './Component';
 
 const publicMethods = [
-	'fetch',
-	'stopFetchByName',
-	'stopFetch',
-	'hasFetching'
+  'fetch',
+  'stopFetchByName',
+  'stopFetch',
+  'hasFetching'
 ];
 
 const {
-	publicMethod
+  publicMethod
 } = Component;
 
 export default class FetchManager extends Component {
 
-	initialization(...args) {
-		super.initialization(...args);
+  initialization(...args) {
+    super.initialization(...args);
 
-		this.fetchingDatastore = {};
-		this.stopKeys = {};
-	}
+    this.fetchingDatastore = {};
+    this.stopKeys = {};
+  }
 
-	destruction() {
-		super.destruction();
+  destruction() {
+    super.destruction();
 
-		Object.values(this.stopKeys).forEach(key => {
-			stopFetchData(key);
-		});
-		this.stopKeys = null;
+    Object.values(this.stopKeys).forEach(key => {
+      stopFetchData(key);
+    });
+    this.stopKeys = null;
 
-		Object.values(this.fetchingDatastore).forEach(index => {
-			clearTimeout(index);
-		});
-		this.fetchingDatastore = null;
-	}
+    Object.values(this.fetchingDatastore).forEach(index => {
+      clearTimeout(index);
+    });
+    this.fetchingDatastore = null;
+  }
 
-	@publicMethod
-	hasFetching() {
-		return hasFetching();
-	}
+  @publicMethod
+  hasFetching() {
+    return hasFetching();
+  }
 
-	@publicMethod
-	fetch(fetcher, data, dataInfo = {}, stop = null) {
-		const stopKey = createUid('stopKey-');
-		this.stopKeys[stopKey] = stopKey;
+  @publicMethod
+  fetch(fetcher, data, dataInfo = {}, stop = null) {
+    const stopKey = createUid('stopKey-');
+    this.stopKeys[stopKey] = stopKey;
 
-		let doStop = () => {
-			this.devLog(`stop fetch  `, fetcher, data, stopKey);
-			this.stopFetch(stopKey);
-		};
+    let doStop = () => {
+      this.devLog(`stop fetch  `, fetcher, data, stopKey);
+      this.stopFetch(stopKey);
+    };
 
-		this.emitter.once(`$$destroy:${this.clazz}=${this.key}`, doStop);
-		if (typeof stop === 'string') {
-			this.emitter.once(`$$data:${stop}`, doStop);
-		} else if (typeof stop === 'function') {
-			stop(doStop);
-		}
+    this.emitter.once(`$$destroy:${this.clazz}=${this.key}`, doStop);
+    if (typeof stop === 'string') {
+      this.emitter.once(`$$data:${stop}`, doStop);
+    } else if (typeof stop === 'function') {
+      stop(doStop);
+    }
 
-		return fetchData(fetcher, data, dataInfo, stopKey).catch(err => {
-			if (this.destroyed) {
-				return;
-			}
+    return fetchData(fetcher, data, dataInfo, stopKey).catch(err => {
+      if (this.destroyed) {
+        return;
+      }
 
-			if (err === ABORT_REQUEST) {
-				this.devLog('abort request: ', fetcher, data, stopKey)
-				return;
-			}
+      if (err === ABORT_REQUEST) {
+        this.devLog('abort request: ', fetcher, data, stopKey)
+        return;
+      }
 
-			return Promise.reject(err);
-		}).finally(() => {
-			if (this.destroyed) {
-				return;
-			}
+      return Promise.reject(err);
+    }).finally(() => {
+      if (this.destroyed) {
+        return;
+      }
 
-			this.stopKeys[stopKey] = null;
-		});
-	}
+      Timer.refreshView();
 
-	@publicMethod
-	stopFetchByName(stop) {
-		this.emitter.emit(`$$data:${stop}`);
-	}
+      this.stopKeys[stopKey] = null;
+    });
+  }
 
-	@publicMethod
-	stopFetch(name) {
-		if (isNvl(name)) {
-			return;
-		}
+  @publicMethod
+  stopFetchByName(stop) {
+    this.emitter.emit(`$$data:${stop}`);
+  }
 
-		if (this.stopKeys[name]) {
-			stopFetchData(this.stopKeys[name]);
-			this.stopKeys[name] = null;
-		}
+  @publicMethod
+  stopFetch(name) {
+    if (isNvl(name)) {
+      return;
+    }
 
-		if (this.fetchingDatastore[name]) {
-			clearTimeout(this.fetchingDatastore[name]);
-			this.fetchingDatastore[name] = null;
-		}
-	}
+    if (this.stopKeys[name]) {
+      stopFetchData(this.stopKeys[name]);
+      this.stopKeys[name] = null;
+    }
 
-	@publicMethod
-	fetchStoreData(param = {}) {
-		const {
-			name = null,
-				data = {},
-				clear = false,
-				force = false,
-				before = udFun,
-				after = udFun,
-		} = param;
+    if (this.fetchingDatastore[name]) {
+      clearTimeout(this.fetchingDatastore[name]);
+      this.fetchingDatastore[name] = null;
+    }
+  }
 
-		clearTimeout(this.fetchingDatastore[name]);
-		this.fetchingDatastore[name] = setTimeout(() => {
-			if (this.destroyed) {
-				return;
-			}
-			const ds = this.dataHub.getDataStore(name);
-			const pagination = ds.paginationManager;
+  @publicMethod
+  fetchStoreData(param = {}) {
+    const {
+      name = null,
+        data = {},
+        clear = false,
+        force = false,
+        before = udFun,
+        after = udFun,
+    } = param;
 
-			const {
-				fetcher = null
-			} = ds.getStoreConfig();
+    clearTimeout(this.fetchingDatastore[name]);
+    this.fetchingDatastore[name] = setTimeout(() => {
+      if (this.destroyed) {
+        return;
+      }
+      const ds = this.dataHub.getDataStore(name);
+      const pagination = ds.paginationManager;
 
-			if (!fetcher) {
-				this.devLog(`fetchStoreData failed: store=${name} no fetcher.`);
-				return;
-			}
+      const {
+        fetcher = null
+      } = ds.getStoreConfig();
 
-			if (ds.isLocked()) {
-				this.errLog(`can't fetch ${name} when it is locked`);
-				return;
-			}
+      if (!fetcher) {
+        this.devLog(`fetchStoreData failed: store=${name} no fetcher.`);
+        return;
+      }
 
-			if (!force && ds.isLoading()) {
-				this.errLog(`can't fetch ${name} when it is loading`);
-				return;
-			}
+      if (ds.isLocked()) {
+        this.errLog(`can't fetch ${name} when it is locked`);
+        return;
+      }
 
-			pagination.stopFetch();
-			ds.clearLoading();
-			this.stopFetch(name);
+      if (!force && ds.isLoading()) {
+        this.errLog(`can't fetch ${name} when it is loading`);
+        return;
+      }
 
-			const stopKey = this.stopKeys[name] = createUid('stopKey-');
-			if (clear) {
-				before();
-				ds.clear();
-				pagination.setCount(0);
-				after();
-				return;
-			}
+      pagination.stopFetch();
+      ds.clearLoading();
+      this.stopFetch(name);
 
-			const pagePromise = pagination.fetch(data);
-			const pageInfo = pagination.getPageInfo();
+      const stopKey = this.stopKeys[name] = createUid('stopKey-');
+      if (clear) {
+        before();
+        ds.clear();
+        pagination.setCount(0);
+        after();
+        return;
+      }
 
-			const dataInfo = {
-				dataStore: true,
-				name,
-				...pageInfo
-			};
+      const pagePromise = pagination.fetch(data);
+      const pageInfo = pagination.getPageInfo();
 
-			before();
-			ds.loading();
+      const dataInfo = {
+        dataStore: true,
+        name,
+        ...pageInfo
+      };
 
-			let resultData = [];
-			let errorMsg = null;
+      before();
+      ds.loading();
 
-			if (pageInfo.merge) {
-				data[pageInfo.pageNumberField] = pagination.page;
-				data[pageInfo.pageSizeField] = pagination.size;
-			}
+      let resultData = [];
+      let errorMsg = null;
 
-			// fetcher, data = null, dataInfo = {}, stopKey = null
-			const dataPromise = fetchData(fetcher, data, dataInfo, stopKey)
-				.then(result => {
-					resultData = result;
-				})
-				.catch(err => {
-					errorMsg = err
-				});
+      if (pageInfo.merge) {
+        data[pageInfo.pageNumberField] = pagination.page;
+        data[pageInfo.pageSizeField] = pagination.size;
+      }
 
-			Promise
-				.all([dataPromise, pagePromise])
-				.finally(() => {
-					if (!this.destroyed) {
-						this.stopKeys[name] = null;
-						if (errorMsg !== null) {
-							ds.clearLoading();
-							if (errorMsg !== ABORT_REQUEST) {
-								ds.setErrorMsg(errorMsg);
-							}
-						} else {
-							ds.loaded(resultData);
-						}
-					}
-					after();
-				});
+      // fetcher, data = null, dataInfo = {}, stopKey = null
+      const dataPromise = fetchData(fetcher, data, dataInfo, stopKey)
+        .then(result => {
+          resultData = result;
+        })
+        .catch(err => {
+          errorMsg = err
+        });
 
-		}, getRefreshRate());
-	}
+      Promise
+        .all([dataPromise, pagePromise])
+        .finally(() => {
+          if (!this.destroyed) {
+            this.stopKeys[name] = null;
+            if (errorMsg !== null) {
+              ds.clearLoading();
+              if (errorMsg !== ABORT_REQUEST) {
+                ds.setErrorMsg(errorMsg);
+              }
+            } else {
+              ds.loaded(resultData);
+            }
+          }
+          after();
+        });
+
+    }, getRefreshRate());
+  }
 }
 
 FetchManager.publicMethods = publicMethods;
